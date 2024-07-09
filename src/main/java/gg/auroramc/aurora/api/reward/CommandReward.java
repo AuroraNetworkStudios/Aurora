7ca9e63a9731ba2cbe8e9bc5434048b16d469f39
Erik Szabó <erik1szabo1@gmail.com>
2024-07-09 06:25:45 +0200

    Multiple commands in command rewards

--- src/main/java/gg/auroramc/aurora/api/reward/CommandReward.java
+++ src/main/java/gg/auroramc/aurora/api/reward/CommandReward.java
@@ -4,34 +4,42 @@ import gg.auroramc.aurora.Aurora;
 import gg.auroramc.aurora.api.command.CommandDispatcher;
 import gg.auroramc.aurora.api.expression.BooleanExpression;
 import gg.auroramc.aurora.api.message.Placeholder;
+import lombok.Getter;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
 public class CommandReward extends AbstractReward {
-    private String command;
+    @Getter
+    private List<String> commands;
     private String correctionExpression;
 
     @Override
     public void execute(Player player, long level, List<Placeholder<?>> formulaPlaceholders) {
-        if (command == null) return;
-        CommandDispatcher.dispatch(player, command, formulaPlaceholders);
+        if (commands == null) return;
+        for (String command : commands) {
+            CommandDispatcher.dispatch(player, command, formulaPlaceholders);
+        }
     }
 
     @Override
     public void init(ConfigurationSection args) {
         super.init(args);
-        command = args.getString("command", null);
-        correctionExpression = args.getString("correction-condition");
-
-        if (command == null) {
-            Aurora.logger().warning("CommandReward has no command key");
+        if(args.isString("command") && args.getString("command") != null) {
+            commands = List.of(args.getString("command"));
+        } else if(args.isList("command")) {
+            commands = args.getStringList("command");
+        } else {
+            commands = List.of();
+            Aurora.logger().warning("CommandReward doesn't have the command key");
         }
+
+        correctionExpression = args.getString("correction-condition");
     }
 
     public boolean shouldBeCorrected(Player player, long level) {
-        if (correctionExpression == null || command == null) return false;
+        if (correctionExpression == null || commands == null) return false;
         return BooleanExpression.eval(player, correctionExpression, Placeholder.of("{level}", level));
     }
 }
