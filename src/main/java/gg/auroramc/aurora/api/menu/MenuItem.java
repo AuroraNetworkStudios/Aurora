90de48ca4c5966ed48363535920d3f55d4c5e16c
Erik Szabó <erik1szabo1@gmail.com>
2024-08-31 18:37:37 +0200

    Add {player} placeholder to menu item click actions

--- src/main/java/gg/auroramc/aurora/api/menu/MenuItem.java
+++ src/main/java/gg/auroramc/aurora/api/menu/MenuItem.java
@@ -1,6 +1,7 @@
 package gg.auroramc.aurora.api.menu;
 
 import gg.auroramc.aurora.api.command.CommandDispatcher;
+import gg.auroramc.aurora.api.message.Placeholder;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -22,26 +23,29 @@ public class MenuItem {
 
     @Deprecated(forRemoval = true)
     public void applyToInventory(Inventory inventory) {
-        if(itemBuilder.getConfig().getSlot() != null) {
+        if (itemBuilder.getConfig().getSlot() != null) {
             inventory.setItem(itemBuilder.getConfig().getSlot(), itemStack);
-        } else if(itemBuilder.getConfig().getSlots() != null && !itemBuilder.getConfig().getSlots().isEmpty()) {
+        } else if (itemBuilder.getConfig().getSlots() != null && !itemBuilder.getConfig().getSlots().isEmpty()) {
             itemBuilder.getConfig().getSlots().forEach(s -> inventory.setItem(s, itemStack));
         }
     }
 
     public void runOnClickCommands(Player player) {
-        if(itemBuilder.getConfig().getOnClick() == null) return;
-        itemBuilder.getConfig().getOnClick().forEach(c -> CommandDispatcher.dispatch(player, c));
+        if (itemBuilder.getConfig().getOnClick() == null) return;
+        var placeholder = Placeholder.of("{player}", player.getName());
+        itemBuilder.getConfig().getOnClick().forEach(c -> CommandDispatcher.dispatch(player, c, placeholder));
     }
 
     public void runOnLeftClickCommands(Player player) {
-        if(itemBuilder.getConfig().getOnLeftClick() == null) return;
-        itemBuilder.getConfig().getOnLeftClick().forEach(c -> CommandDispatcher.dispatch(player, c));
+        if (itemBuilder.getConfig().getOnLeftClick() == null) return;
+        var placeholder = Placeholder.of("{player}", player.getName());
+        itemBuilder.getConfig().getOnLeftClick().forEach(c -> CommandDispatcher.dispatch(player, c, placeholder));
     }
 
     public void runOnRightClickCommands(Player player) {
-        if(itemBuilder.getConfig().getOnRightClick() == null) return;
-        itemBuilder.getConfig().getOnRightClick().forEach(c -> CommandDispatcher.dispatch(player, c));
+        if (itemBuilder.getConfig().getOnRightClick() == null) return;
+        var placeholder = Placeholder.of("{player}", player.getName());
+        itemBuilder.getConfig().getOnRightClick().forEach(c -> CommandDispatcher.dispatch(player, c, placeholder));
     }
 
     public void refresh() {
