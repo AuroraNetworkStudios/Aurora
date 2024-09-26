592a52bf4936c32434fee53b15f764dd05061675
Erik Szabó <erik1szabo1@gmail.com>
2024-09-26 12:19:22 +0200

    Add default value for item in MatcherConfigs to prevent NPE with the Dummy matcher

--- src/main/java/gg/auroramc/aurora/api/config/premade/ConcreteMatcherConfig.java
+++ src/main/java/gg/auroramc/aurora/api/config/premade/ConcreteMatcherConfig.java
@@ -3,6 +3,7 @@ package gg.auroramc.aurora.api.config.premade;
 import lombok.Getter;
 import org.bukkit.configuration.ConfigurationSection;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,5 +12,5 @@ public class ConcreteMatcherConfig {
     private List<String> inheritsFrom;
     private ConfigurationSection rewards;
     private Integer level;
-    private Map<String, ItemConfig> item;
+    private Map<String, ItemConfig> item = new HashMap<>();
 }
