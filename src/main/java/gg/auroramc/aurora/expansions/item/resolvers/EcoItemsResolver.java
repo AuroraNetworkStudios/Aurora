37af96361a4d266bcb3c636a1babeb65a28aa64b
Erik Szabó <erik1szabo1@gmail.com>
2024-12-06 02:09:40 +0100

    Add EcoArmor to the experimental PDC item resolver

--- src/main/java/gg/auroramc/aurora/expansions/item/resolvers/EcoItemsResolver.java
+++ src/main/java/gg/auroramc/aurora/expansions/item/resolvers/EcoItemsResolver.java
@@ -5,15 +5,20 @@ import gg.auroramc.aurora.api.item.ItemResolver;
 import gg.auroramc.aurora.api.item.TypeId;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Locale;
 
 public class EcoItemsResolver implements ItemResolver {
     private final NamespacedKey ecoitemsKey = new NamespacedKey("ecoitems", "item");
     private final NamespacedKey ecoarmorKey = new NamespacedKey("ecoarmor", "set");
     // 0 if it isn't advanced
     private final NamespacedKey ecoarmorAdvancedKey = new NamespacedKey("ecoarmor", "advanced");
+    private final NamespacedKey ecoarmorShardKey = new NamespacedKey("ecoarmor", "advancement-shard");
+    private final NamespacedKey ecoarmorUpgradeCrystalKey = new NamespacedKey("ecoarmor", "upgrade_crystal");
     private final NamespacedKey talismansKey = new NamespacedKey("talismans", "talisman");
     private final NamespacedKey ecopetsKey = new NamespacedKey("ecopets", "pet_egg");
     private final NamespacedKey reforgesKey = new NamespacedKey("reforges", "reforge_stone");
@@ -40,13 +45,17 @@ public class EcoItemsResolver implements ItemResolver {
     private boolean isEcoItem(ItemStack item) {
         // Check every single pdc key
         // This is so dumb
-        return item.getPersistentDataContainer().has(ecoitemsKey) ||
-                item.getPersistentDataContainer().has(ecoarmorKey) ||
-                item.getPersistentDataContainer().has(talismansKey) ||
-                item.getPersistentDataContainer().has(ecopetsKey) ||
-                item.getPersistentDataContainer().has(reforgesKey) ||
-                item.getPersistentDataContainer().has(ecoscrollsKey) ||
-                item.getPersistentDataContainer().has(ecocratesKey);
+        var pdc = item.getPersistentDataContainer();
+
+        return pdc.has(ecoitemsKey) ||
+                pdc.has(ecoarmorKey) ||
+                pdc.has(talismansKey) ||
+                pdc.has(ecopetsKey) ||
+                pdc.has(reforgesKey) ||
+                pdc.has(ecoscrollsKey) ||
+                pdc.has(ecocratesKey) ||
+                pdc.has(ecoarmorShardKey) ||
+                pdc.has(ecoarmorUpgradeCrystalKey);
     }
 
     private TypeId resolveEcoItemId(ItemStack item) {
@@ -57,15 +66,11 @@ public class EcoItemsResolver implements ItemResolver {
         if (pdc.has(ecoitemsKey)) {
             return new TypeId("eco", "ecoitems:" + pdc.get(ecoitemsKey, type));
         } else if (pdc.has(ecoarmorKey)) {
-            // Lord have mercy, I can't figure out the <slot>, so for now we use the dumb method and pray to god
-            var customItem = Items.getCustomItem(item);
-            if (customItem != null) {
-                return new TypeId("eco", customItem.getKey().toString());
+            var slot = parseArmorSlot(item);
+            if(pdc.has(ecoarmorAdvancedKey) && pdc.get(ecoarmorAdvancedKey, PersistentDataType.INTEGER) != 0) {
+                return new TypeId("eco", "ecoarmor:set_" + pdc.get(ecoarmorKey, type) + "_" + slot + "_advanced");
             }
-            //if(pdc.has(ecoarmorAdvancedKey) && pdc.get(ecoarmorAdvancedKey, PersistentDataType.INTEGER) != 0) {
-            //    return new TypeId("eco", "ecoarmor:set_" + pdc.get(ecoarmorKey, type) + "_slot_" + "_advanced");
-            //}
-            //return new TypeId("eco", "ecoarmor:set_" + pdc.get(ecoarmorKey, type) + "_slot");
+            return new TypeId("eco", "ecoarmor:set_" + pdc.get(ecoarmorKey, type) + "_" + slot);
         } else if (pdc.has(talismansKey)) {
             return new TypeId("eco", "talismans:" + pdc.get(talismansKey, type));
         } else if (pdc.has(ecopetsKey)) {
@@ -76,7 +81,58 @@ public class EcoItemsResolver implements ItemResolver {
             return new TypeId("eco", "ecoscrolls:scroll_" + pdc.get(ecoscrollsKey, type));
         } else if (pdc.has(ecocratesKey)) {
             return new TypeId("eco", "ecocrates:" + pdc.get(ecocratesKey, type) + "_key");
+        } else if(pdc.has(ecoarmorShardKey)) {
+            return new TypeId("eco", "ecoarmor:shard_" + pdc.get(ecoarmorShardKey, type));
+        } else if(pdc.has(ecoarmorUpgradeCrystalKey)) {
+            return new TypeId("eco", "ecoarmor:upgrade_crystal_" + pdc.get(ecoarmorUpgradeCrystalKey, type));
         }
         return null;
+    }
+
+    private String parseArmorSlot(ItemStack item) {
+        return ArmorSlot.getSlot(item).name().toLowerCase(Locale.getDefault());
+    }
+
+
+    public enum ArmorSlot {
+        HELMET(EquipmentSlot.HEAD),
+        CHESTPLATE(EquipmentSlot.CHEST),
+        ELYTRA(EquipmentSlot.CHEST),
+        LEGGINGS(EquipmentSlot.LEGS),
+        BOOTS(EquipmentSlot.FEET);
+
+        private final EquipmentSlot slot;
+
+        ArmorSlot(EquipmentSlot slot) {
+            this.slot = slot;
+        }
+
+        public EquipmentSlot getSlot() {
+            return slot;
+        }
+
+        public static ArmorSlot getSlot(ItemStack itemStack) {
+            if (itemStack == null) {
+                return null;
+            }
+            String materialName = itemStack.getType().name();
+            return getSlot(materialName);
+        }
+
+        public static ArmorSlot getSlot(String name) {
+            if (name.contains("HELMET") || name.contains("HEAD") || name.contains("SKULL") || name.contains("PUMPKIN")) {
+                return HELMET;
+            } else if (name.contains("CHESTPLATE")) {
+                return CHESTPLATE;
+            } else if (name.contains("ELYTRA")) {
+                return ELYTRA;
+            } else if (name.contains("LEGGINGS")) {
+                return LEGGINGS;
+            } else if (name.contains("BOOTS")) {
+                return BOOTS;
+            } else {
+                return null;
+            }
+        }
     }
 }
